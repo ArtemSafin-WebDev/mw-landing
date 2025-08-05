1f2fedf7ad2094e133f2c23a994f2133ac14fd0c
Artem Safin <artemsafin.webdev@gmail.com>
2025-08-05 21:37:50 +0300

    По проектам

--- src/js/projects.ts
+++ src/js/projects.ts
@@ -13,10 +13,11 @@ export default function projects() {
       element.querySelectorAll<HTMLElement>(".projects__card")
     );
     cards.forEach((card) => {
+      if (card.closest(".projects__tab:not(.active)")) return;
       const tl = gsap.timeline({
         scrollTrigger: {
           trigger: card,
-          start: "top bottom-=30%",
+          start: "top bottom-=20%",
           end: "bottom top",
           markers: false,
           scrub: false,
@@ -46,5 +47,40 @@ export default function projects() {
         return;
       filtersBtn?.nextElementSibling?.classList.remove("active");
     });
+
+    if (element.matches(".projects:not(.projects--archive)")) {
+      const currentCategoryBtn = element.querySelector<HTMLLinkElement>(
+        ".projects__all-link"
+      );
+      const tabs = Array.from(
+        element.querySelectorAll<HTMLElement>(".projects__tab")
+      );
+      const links = Array.from(
+        element.querySelectorAll<HTMLLinkElement>(".projects__categories-link")
+      );
+
+      const setActiveLink = (link: HTMLLinkElement) => {
+        if (currentCategoryBtn) currentCategoryBtn.href = link.href;
+        const linkIndex = links.indexOf(link);
+        links.forEach((link) => link.classList.remove("active"));
+        link.classList.add("active");
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabs[linkIndex]?.classList.add("active");
+        ScrollTrigger.refresh();
+      };
+
+      links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+          event.preventDefault();
+          setActiveLink(link);
+        });
+      });
+
+      const initiallyActiveLink = links.find((link) =>
+        link.classList.contains("active")
+      );
+
+      if (initiallyActiveLink) setActiveLink(initiallyActiveLink);
+    }
   });
 }
